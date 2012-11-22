b0520cb028af5840a1dcc49fff6f306ac76fda39
Ryan Sanche <ryansanche@streamon.fm>
2012-11-21 17:00:56 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Synphony::Application.config.secret_token = '0996775f9c9e152eb2b8643dd3aca8a95a090f7c2d1ac6dc55b0207afb0d929913081604af224790206d5b691877476709014800b582717bff6b9ea476e047ba'
