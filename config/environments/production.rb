786da3b592ec39e156f729009a0e3f9b3f4ef4d1
Ryan Sanche <ryansanche@streamon.fm>
2012-11-30 08:30:50 -0800

    Fixed email going as example.com.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Synphony::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'helpchildrenread.org' }
 end
